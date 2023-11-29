3ad2b5b2abce49b66f0f46b392ea467dab7432ba
NiekH <567837@edu.rocmn.nl>
2023-11-29 09:23:38 +0100

    Email manager / sendemail gemaakt

--- private/managers/EmailManager.php
+++ private/managers/EmailManager.php
@@ -6,27 +6,28 @@ use PHPMailer\PHPMailer\Exception;
 require '../static/PHPMailer/Exception.php';
 require '../static/PHPMailer/PHPMailer.php';
 require '../static/PHPMailer/SMTP.php';
-require '..'
+require '../../static/autoloader.php';
 class EmailManager
 {
   public static function sendGameMail($id, $html)
   {
+    global $config;
     $mail = new PHPMailer(true);
 
     try {
       //Server settings
       //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
       $mail->isSMTP();                                            //Send using SMTP
-      $mail->Host       = $config["Host"];                   //Set the SMTP server to send through
+      $mail->Host       = $config->smtp->host;                   //Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-      $mail->Username   = $config["Username"];                       //SMTP username
-      $mail->Password   = $config["Password"];                                 //SMTP password
+      $mail->Username   = $config->smtp->username;                      //SMTP username
+      $mail->Password   = $config->smtp->password;                                 //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-      $mail->Port       = $config["Port"];                                       //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+      $mail->Port       = $config->smtp->port;                                       //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
       //Recipients
       $mail->setFrom('student@ictcampus.nl', 'Ratjetoe');
-      $mail->addAddress($values->email);     //Add a recipient
+      // $mail->addAddress($values->email);     //Add a recipient
 
       //Content
       $mail->isHTML(true);                                  //Set email format to HTML
