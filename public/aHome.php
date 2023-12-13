7af458874e1a57cb6c16f5cba6eea1d39359b25f
Ruben Boter <boter.ruben@gmail.com>
2023-12-13 08:59:04 +0100

    voorbeeld table af

--- public/aHome.php
+++ public/aHome.php
@@ -8,33 +8,73 @@ $Song = SongManager::getAll();
 
 <head>
   <?php include "../private/components/head.php" ?>
+  <style>
+    body {
+      background-color: white;
+    }
+
+    .container {
+      margin-top: 20px;
+    }
+
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    th,
+    td {
+      padding: 8px;
+      text-align: left;
+    }
+
+    tr {
+      border: 1px solid black;
+
+    }
+
+    th {
+      background-color: black;
+      color: white;
+    }
+
+    tr:nth-child(odd) {
+      background-color: rgb(216, 34, 34);
+      color: white;
+    }
+
+    tr:nth-child(even) {
+      background-color: white;
+      color: black;
+    }
+  </style>
 </head>
 
-<body style="background-color: white;">
+<body>
   <?php include "../private/components/adminNav.php" ?>
   <div class="container">
     <h1>Home pagina</h1>
 
-    <table class="table table-striped">
-      <thead class="table-dark">
+    <table>
+      <thead>
         <tr>
-          <th scope="col"><img src="../img/logo/npo_radio2_logo.svg" alt="" width="50" height="50"></th>
-          <th scope="col">Naam</th>
-          <th scope="col">Verborgen</th>
+          <th><img src="../img/logo/npo_radio2_logo.svg" alt="" width="50" height="50"></th>
+          <th>Naam</th>
+          <th>Verborgen</th>
         </tr>
       </thead>
       <tbody>
         <?php
         foreach ($Song as $s) {
           echo "<tr>";
-          echo "<td>$s->id</td>";
+          echo "<td scope='row'>$s->id</td>";
           echo "<td>$s->name</td>";
           if ($s->is_hidden == 0) {
             echo "<td>Nee</td>";
           } else {
             echo "<td>Ja</td>";
           }
-          echo "</tb>";
+          echo "</tr>";
         }
         ?>
       </tbody>
@@ -43,10 +83,3 @@ $Song = SongManager::getAll();
 </body>
 
 </html>
-
-<style>
-  .table {
-    --bs-table-striped-color: white;
-    --bs-table-striped-bg: rgb(216 34 34);
-  }
-</style>
